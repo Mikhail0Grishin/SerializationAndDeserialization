50ad87b2e89b6c5cc66312c9517acf337bda03d2
Mikhail0Grishin <mikhail0910grishin@gmail.com>
2021-09-25 14:33:09 +0300

    Adding interface and element of list

---
+++ SerializationAndDeserialization/IListSerializer.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace SerializationAndDeserialization
+{
+    interface IListSerializer
+    {
+        Task Serialize(ListNode head, Stream s);
+
+        Task<ListNode> Deserialaze(Stream s);
+
+        Task<ListNode> DeepCopy(ListNode head);
+
+    }
+}
---
+++ SerializationAndDeserialization/ListNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationAndDeserialization
+{
+    class ListNode
+    {
+        public ListNode Previous;
+
+        public ListNode Next;
+
+        public ListNode Random;
+
+        public string Data;
+
+    }
+}
